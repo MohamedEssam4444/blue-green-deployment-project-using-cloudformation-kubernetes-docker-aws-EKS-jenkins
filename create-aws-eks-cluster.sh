840ea76bf3d981832764265e50af0b6136a1bc6f
MohamedEssam4444 <68178003+MohamedEssam4444@users.noreply.github.com>
2020-11-02 12:59:17 +0200

    Create create-aws-eks-cluster.sh

---
+++ create-aws-eks-cluster.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+#step1 create aws eks cluster
+eksctl create cluster \
+  --name jenkinstest \
+  --version <1.18> \
+  --node-type t2.micro \
+  --nodes 3 \
+  --nodes-min 1 \
+  --nodes-max 4 \
+  --managed
+#step2 create kubeconfig file to store info of cluster so we can access it later and communicate with it using kubectl 
+aws eks --region us-east-2 update-kubeconfig --name jenkinstest
